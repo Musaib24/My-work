24ba9aa833e9727b256f96391fbb98122575eb1e
sysadmin <sysadmin@ibpomke-imac044-4.local>
2018-05-29 13:28:19 -0400

    Initial Commit

---
+++ Guess Number/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guess Number
+//
+//  Created by sysadmin on 5/29/18.
+//  Copyright © 2018 sysadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Guess Number/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guess Number
+//
+//  Created by sysadmin on 5/29/18.
+//  Copyright © 2018 sysadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
